82702e548f304e29992dede61a1f42824c2e670d
MRit-ReDo <shambhavi.singh.work@gmail.com>
2025-09-18 18:55:01 +0530

    create update cart functionality

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import {
     cart,
     deleteFromCheckoutPage,
     findProductByID,
-    cartTotal
+    saveUpdateFromCheckoutPage,
 } from "./utils/cart.js";
 import { 
     formatCurrency,
@@ -32,8 +32,12 @@ const renderCheckoutPage = () => {
                         <div class="product-name">${product.name}</div>
                         <div class="product-price">$${formatCurrency(product.priceCents)}</div>
                         <div class="product-quantity">
-                            <span>Quantity: <span class="quantity-label">${cartItem.qty}</span></span>
-                            <span class="update-quantity-link link-primary" data-id="${product.id}">Update</span>
+                            <span>Quantity: <span class="quantity-label-${product.id}">${cartItem.qty}</span></span>
+                            <span class="update-quantity-link link-primary update-quantity-link-${product.id}" data-id="${product.id}">Update</span>
+                            <span class="save-quantity-link link-primary save-quantity-link-${product.id} hide" data-id="${product.id}">
+                                <input type="text" class="save-quantity-input save-quantity-input-${product.id}" placeholder="1-100" style="width: 40px">
+                                <span class="save-quantity-button save-quantity-button-${product.id}" data-id="${product.id}">Save</span>
+                            </span>
                             <span class="delete-quantity-link link-primary" data-id="${product.id}">Delete</span>
                         </div>
                     </div>
@@ -89,3 +93,25 @@ document.querySelectorAll(".delete-quantity-link").forEach((button) => {
         updateBill(totalDeliveryCharge());
     })
 })
+
+document.querySelectorAll(".update-quantity-link").forEach((button) => {
+    button.addEventListener("click",() => {
+        const id = button.dataset.id;
+        document.querySelector(".update-quantity-link-"+id).classList.add("hide");
+        document.querySelector(".save-quantity-link-"+id).classList.remove("hide");
+    })
+})
+
+document.querySelectorAll(".save-quantity-button").forEach((button) => {
+    button.addEventListener("click",() => {
+        const id = button.dataset.id;
+        saveUpdateFromCheckoutPage(id);
+        updateNumberOfItems();
+        updateBill(totalDeliveryCharge());
+        document.querySelector(".update-quantity-link-"+id).classList.remove("hide");
+        document.querySelector(".save-quantity-input-"+id).value = "";
+        document.querySelector(".save-quantity-link-"+id).classList.add("hide");
+        
+    })
+})
+
